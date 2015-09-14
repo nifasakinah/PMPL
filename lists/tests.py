6d974fb9c46877439491f1f7dcfe54488dbcad4f
Hanifah <nifasakinah@gmail.com>
2015-09-14 11:45:39 +0700

    Refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,20 +1,8 @@
-from django.core.urlresolvers import resolve
-from django.test import TestCase
-from django.http import HttpRequest
-
-from lists.views import home_page
-
-
-class HomePageTest(TestCase):
-
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+from django.template.loader import render_to_string
 
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()  
-        response = home_page(request)  
-        self.assertTrue(response.content.startswith(b'<html>'))  
-        self.assertIn(b'<title>PMPL</title><p>Hanifah Sakinah / 1206278196</p>', response.content)  
-        self.assertTrue(response.content.endswith(b'</html>'))  
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,7 @@
-
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
+#from django.shortcuts import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    return HttpResponse('<html><title>PMPL</title><p>Hanifah Sakinah / 1206278196</p></html>')
+    return render(request, 'home.html')
+    #return HttpResponse('<html><title>PMPL</title><p>Hanifah Sakinah / 1206278196</p></html>')
