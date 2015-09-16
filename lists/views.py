38265af7c9df1e308ad4604571b9484de0b412c4
Hanifah <nifasakinah@gmail.com>
2015-09-16 12:12:53 +0700

    Change in functional_tests.py; home.html; tests.py; views.py

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-#from django.shortcuts import HttpResponse
+from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'home.html')
+    #return render(request, 'home.html')
     #return HttpResponse('<html><title>PMPL</title><p>Hanifah Sakinah / 1206278196</p></html>')
+    #if request.method == 'POST':
+    #    return HttpResponse(request.POST['item_text'])
+    #return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
