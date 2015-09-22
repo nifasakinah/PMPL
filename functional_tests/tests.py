2f34a3456eccca2a8d62fd2eca0886f68826a4b5
Hanifah <nifasakinah@gmail.com>
2015-09-22 10:23:11 +0700

    udah error regex

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,7 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(unittest.TestCase):  
+class NewVisitorTest(LiveServerTestCase):
+#class NewVisitorTest(unittest.TestCase):  
 
     def setUp(self):  
         self.browser = webdriver.Firefox()
@@ -42,6 +43,8 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+') 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         #table = self.browser.find_element_by_id('id_list_table')
@@ -82,5 +85,42 @@ class NewVisitorTest(unittest.TestCase):
 
         # She visits that URL - her to-do list is still there.
 
+        # Satisfied, she goes back to sleep
+        
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        # Now a new user, Francis, comes along to the site.
+
+        ## We use a new browser session to make sure that no information
+        ## of Edith's is coming through from cookies etc #1
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis visits the home page.  There is no sign of Edith's
+        # list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Francis starts a new list by entering a new item. He
+        # is less interesting than Edith...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, there is no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Satisfied, they both go back to sleep
 if __name__ == '__main__':  
     unittest.main(warnings='ignore')  
