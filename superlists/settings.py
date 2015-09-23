a551b0fcdf7099332ce45601eaed66a10ae22322
Hanifah <nifasakinah@gmail.com>
2015-09-23 15:48:06 +0700

    Bismillah deploy

--- superlists/settings.py
+++ superlists/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
+# import os
 
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -104,6 +104,7 @@ USE_TZ = True
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
+DATABASES = {}
 DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
--- superlists/wsgi.py
+++ superlists/wsgi.py
@@ -15,4 +15,4 @@ from dj_static import Cling
 application = Cling(get_wsgi_application())
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superlists.settings")
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
