6eeb7d44f030eadde118efe69f7417db4854b515
Hanifah <nifasakinah@gmail.com>
2015-11-10 14:47:37 +0700

    push homepage

--- lists/views.py
+++ lists/views.py
@@ -4,39 +4,21 @@ from lists.models import Item, List
 
 # Create your views here.
 def home_page(request):
-    #return render(request, 'home.html')
-    #return HttpResponse('<html><title>PMPL</title><p>Hanifah Sakinah / 1206278196</p></html>')
     #if request.method == 'POST':
-    #    return HttpResponse(request.POST['item_text'])
-    #return render(request, 'home.html')
-    #item = Item()
-    #item.text = request.POST.get('item_text', '')
-    #item.save()
+    #    Item.objects.create(text=request.POST['item_text'])
+    #    return redirect('/lists/the-only-list-in-the-world/')
 
-    #if request.method == 'POST':
-
-     #   Item.objects.create(text=request.POST['item_text'])
-      #  return redirect('/lists/the-only-list-in-the-world/')
-
-    items = Item.objects.all()
+    #items = Item.objects.all()
     #countsItem = Item.objects.count()
+    #comment = 'yey, waktunya berlibur'
+
+    return render(request, 'index.html')
+
+def list_page(request):
+    countsItem = Item.objects.count()
     comment = 'yey, waktunya berlibur'
 
-    #if countsItem > 0:
-     #   comment = 'sibuk tapi santai'
-    #if countsItem >= 5:
-     #   comment = 'oh tidak'
     return render(request, 'home.html', {'comment': comment})
-    #return render(request, 'home.html')
-        #new_item_text = request.POST['item_text']  #1
-     
-   #Item.objects.create(text=new_item_text)  #2
-    #else:
-     #   new_item_text = ''  #3
-
-    #return render(request, 'home.html', {
-    #    'new_item_text': new_item_text,
-    #})
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
     #return render(request, 'list.html', {'list': list_})
--- superlists/urls.py
+++ superlists/urls.py
@@ -5,16 +5,11 @@ from lists import urls as list_urls  #2
 
 urlpatterns = [
     url(r'^$', list_views.home_page, name='home'),
+    url(r'^homelist', list_views.list_page, name='list'),
     url(r'^lists/', include(list_urls)),
-#    url(r'^lists/new$', views.new_list, name='new_list'),
- #   url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
-  #  url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
-    #url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
-    #url(r'^lists/(.+)/$', 'lists.views.view_list', name='view_list'),
-    #url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
-    #url(r'^lists/new$', views.new_list, name='new_list'),
-    #url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
-   # url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 ]
+
+
 
 
